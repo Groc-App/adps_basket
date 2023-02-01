1c17e7723a11fc12d7550e02379363916e803d50
Arjun <arjundagar28@gmail.com>
2023-02-01 17:16:09 +0530

    Added product

--- lib/Widgets/Homepage/MostSelling.dart
+++ lib/Widgets/Homepage/MostSelling.dart
@@ -2,6 +2,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:your_basket/Widgets/Homepage/ProductItem.dart';
 
 class MostSelling extends StatelessWidget {
   const MostSelling({super.key});
@@ -33,50 +34,7 @@ class MostSelling extends StatelessWidget {
               ),
             ),
           ),
-          Container(
-            decoration: const BoxDecoration(
-                border: Border(
-                    top: BorderSide(width: 5),
-                    bottom: BorderSide(width: 5),
-                    left: BorderSide(width: 5),
-                    right: BorderSide(width: 5))),
-            height: scHeight * 0.75,
-            child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, childAspectRatio: 0.7),
-                itemCount: 6,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    decoration: const BoxDecoration(
-                        border: Border(
-                            top: BorderSide(width: 5),
-                            bottom: BorderSide(width: 5),
-                            left: BorderSide(width: 5),
-                            right: BorderSide(width: 5))),
-                    child: Card(
-                      color: Colors.amber,
-                      child: Column(children: [
-                        SizedBox(
-                          height: scWidth * 0.25,
-                          child: Image.network(
-                            'https://m.media-amazon.com/images/I/812816L+HkL._SL1500_.jpg',
-                            fit: BoxFit.fitWidth,
-                          ),
-                        ),
-                        const Text('Amul Toned Milk'),
-                        const Text('500 ml'),
-                        Row(
-                          children: [
-                            const Text('Price'),
-                            OutlinedButton(
-                                onPressed: () {}, child: const Text('ADD')),
-                          ],
-                        )
-                      ]),
-                    ),
-                  );
-                }),
-          ),
+          ProductItem(),
         ],
       ),
     );
